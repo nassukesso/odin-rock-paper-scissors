d528920ba5bff5bcd20ca74bd3506c726c9ad02a
Athanasios Grivas <nasos.grivas@gmail.com>
2022-12-08 20:36:14 +0200

    Create function playRound

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,10 +1,54 @@
 function getRandomInt(max) {
+  //returns randomly one of 0, 1,... max-1
   return Math.floor(Math.random() * max);
 }
 
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
-  let choice = choices[getRandomInt(3)];
+  let choice;
+
+  choice = choices[getRandomInt(3)];
 
   return choice;
 }
+
+function stringNormalize(str) {
+  let normalizedStr;
+
+  normalizedStr = str.toLowerCase();
+  normalizedStr =
+    normalizedStr.slice(0, 1).toUpperCase() + normalizedStr.slice(1);
+
+  return normalizedStr;
+}
+
+function playRound(playerSelection, computerSelection) {
+  const playerSelectionCI = stringNormalize(playerSelection);
+
+  if (playerSelectionCI === computerSelection) return "Tie!";
+
+  switch (playerSelectionCI) {
+    case "Rock":
+      switch (computerSelection) {
+        case "Paper":
+          return "You lose! Paper beats Rock.";
+        case "Scissors":
+          return "You win! Rock beats Scissors."
+      }
+    case "Paper":
+      switch (computerSelection) {
+        case "Rock":
+          return "You win! Paper beats Rock.";
+        case "Scissors":
+          return "You lose! Scissors beats Paper."
+      }
+    case "Scissors":
+      switch (computerSelection) {
+        case "Paper":
+          return "You win! Scissors beats Paper.";
+        case "Rock":
+          return "You lose! Rock beats Scissors.";
+      }
+  }
+}
+
